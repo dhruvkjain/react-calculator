e2a699d8ba395f4a30d63ad08e292359c0a27215
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2023-09-11 13:03:28 +0530

    Fixed Calculation

--- src/App.js
+++ src/App.js
@@ -21,50 +21,35 @@ class App extends React.Component{
     }
     onButtonPress = (event)=>{
         this.setState((prevState)=>{return {display:prevState.display+event.target.innerHTML}})
-        console.log(this.state.display);
-        // if(event.target.innerHTML === "/" || event.target.innerHTML === "+" || event.target.innerHTML === "-" || event.target.innerHTML === "×"){
-        //     console.log(event.target.innerHTML); 
-        //     this.setState({operation:event.target.innerHTML});
-        //     this.setState({history:this.state.display+event.target.innerHTML})
-        //     this.setState({display:""});
-        // }
-        // if(this.state.operation === null){
-        //     this.setState({num1: this.state.num1+event.target.innerHTML});
-        //     console.log(this.state.num1);
-        // }
     }
     evaluate=(event)=>{
+        this.setState({num1:this.state.display})
         this.setState({operation:event.target.innerText});
-        // console.log(this.state.operation);
-        this.setState({num1:this.state.display});
-        console.log(this.state.num1);
+        this.setState({history:this.state.display+event.target.innerHTML});
+        this.setState({display:""});
+        setInterval(()=>{
+            this.setState({num2: this.state.display});
+        });
     }
     allClear=()=>{
-        this.setState({display:"",num1:"",num2:"",operation:null})
+        this.setState({display:"",num1:"",num2:"",operation:null,history:""})
     }
     removeLast=()=>{
         this.setState({display:this.state.display.substring(0,(this.state.display.length-1))})
     }
     equate=()=>{
-        this.setState({num2: this.state.display})
-        console.log(this.state.num1);
-        console.log(this.state.num2);
         try {
             if(this.state.operation === "/"){
-                // this.setState({display:(Number(this.state.num1)/Number(this.state.num2))});
-                this.setState({display:(this.state.num1+this.state.num2)});
+                this.setState({display:(Number(this.state.num1)/Number(this.state.num2))});
             }
             if(this.state.operation === "-"){
-                // this.setState({display:(Number(this.state.num1)-Number(this.state.num2))});
-                this.setState({display:(this.state.num1+this.state.num2)});
+                this.setState({display:(Number(this.state.num1)-Number(this.state.num2))});
             }
             if(this.state.operation === "+"){
-                // this.setState({display:(Number(this.state.num1)+Number(this.state.num2))});
-                this.setState({display:(this.state.num1+this.state.num2)});
+                this.setState({display:(Number(this.state.num1)+Number(this.state.num2))});
             }
             if(this.state.operation === "×"){
-                // this.setState({display:(Number(this.state.num1)*Number(this.state.num2))});
-                this.setState({display:(this.state.num1+this.state.num2)});
+                this.setState({display:(Number(this.state.num1)*Number(this.state.num2))});
             }
             this.setState({history:""})
         }
@@ -84,10 +69,6 @@ class App extends React.Component{
 
                     <Button id="spanac" text={this.state.textac} onPress={this.allClear}/>
                     <Button text={this.state.textdel} onPress={this.removeLast}/>
-                    {/* {this.state.btnvalues.forEach(value =>{
-                        <Button text={value} onPress={this.onButtonPressPress}/>
-                    })} */} 
-                    {/* above loop NOT WORKING */}
                     <Button text={this.state.btnvalues[2]} onPress={this.evaluate} />
                     <Button text={this.state.btnvalues[3]} onPress={this.onButtonPress}/>
                     <Button text={this.state.btnvalues[4]} onPress={this.onButtonPress}/>
